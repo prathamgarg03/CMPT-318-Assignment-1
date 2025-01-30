63d5fec760f2f1e3e84fe0eeb39ffb40abafc4d5
Ruben <duaruben18@gmail.com>
2025-01-30 14:44:58 -0800

    Update Part1.R

--- Part1.R
+++ Part1.R
@@ -1,1 +1,8 @@
 data <- read.table("Group_Assignment_Dataset.txt", header = TRUE, sep = "\t")
+
+data$newDate <- as.POSIXlt(data$Date, format = "%d/%m/%Y") 
+data$week <- strftime(data$newDate, format = "%W")
+groupData <- subset(data, week == "04")
+
+
+
